9e99ae20e2efc8d40f969b33b66d10282c81a7e7
jacobbien <jbien@usc.edu>
2024-04-11 13:59:24 -0700

    do rendering in temporary directory
    - In previous approach, where `litr_render_tmp.qmd` is made, the html file ends up having references to the "_render_tmp" path, which messes up the quarto rendering.  This fixes that since the qmd file name is unchanged (only its location changes).
    - Also added litr-hash checks though if this will be called from litr::render() rather than being an exported function, then we can remove those lines.

--- quarto_render.R
+++ quarto_render.R
@@ -3,7 +3,7 @@ q_render_ <- function(input, package_dir, minimal_eval, ...) {
   input_str <- readLines(input)
   to_inject <- glue::glue(
     '
-  ```{r, echo=FALSE, message=FALSE}
+  ```{r, include=FALSE}
   litr:::setup("{{package_dir}}", {{minimal_eval}})
   ```
   ',
@@ -11,28 +11,47 @@ q_render_ <- function(input, package_dir, minimal_eval, ...) {
     .close = "}}"
   )
   
-  # inject litr::setup code after the 2nd --- of the YAML front matter
-  yaml_end_idx <- which(input_str == "---")[2]
-  changed_str <-
-    c(input_str[1:yaml_end_idx], to_inject, input_str[(yaml_end_idx + 1):length(input_str)])
-  # write modified code to a tmp file
-  tmp_file <- "litr_render_tmp.qmd"
+  # inject litr::setup code after the 2nd --- of the YAML front matter if it's there
+  yaml_marks <- which(input_str == "---")
+  if (length(yaml_marks) >= 2) {
+    yaml_end_idx <- which(input_str == "---")[2]
+    changed_str <- c(input_str[1:yaml_end_idx], 
+                     to_inject, 
+                     input_str[(yaml_end_idx + 1):length(input_str)])
+  } else {
+    stop(litr:::make_noticeable('add yaml header that includes at least the following:
+---
+params:
+  package_name: "{package_dir}"
+  package_parent_dir: "."
+---'))
+  }
+  
+  # create a tmp directory and copy everything from current directory into it
+  # - we want all files there since if it's a book there may be multiple qmd files and other resources; also if the R package already exists we want the litr-hash checks to compare against it
+  # - we want to do everything in a new directory rather than modifying the name of the input file so that the html files will have the correct path
+  tmpdir <- tempfile()
+  fs::dir_create(tmpdir)
+  cat(tmpdir)
+  fs::dir_copy(".", tmpdir)
+  # delete hidden files:
+  fs::file_delete(list.files(all.files=TRUE, pattern = "^\\.[^\\.]"))
+
+  # write modified code to file in there
+  tmp_file <- file.path(tmpdir, input)
   writeLines(changed_str, tmp_file)
+
   # pass tmp file to quarto_render
   out <- quarto::quarto_render(tmp_file, ...)
   # clean up files after quarto finishes rendering
-  # rename the output file to match the name of the original file
+  # remove the modified qmd file and then move the output out of tmpdir
   fs::file_delete(tmp_file)
-  output_file_base_name <- fs::path_ext_remove(tmp_file)
-  input_file_base_name <- fs::path_ext_remove(input)
-  # This assumes the output is always an html file, will need to do something different for bookdown
-  fs::file_move(paste0(output_file_base_name, ".html"),
-                paste0(input_file_base_name, ".html"))
-  # rename the files directory as well
-  # apparently there isn't a built in command to do this in R?
-  system(paste(
-    "mv",
-    paste0(output_file_base_name, "_files"),
-    paste0(input_file_base_name, "_files")
-  ))
+  fs::dir_copy(tmpdir, ".", overwrite = TRUE)
+  fs::dir_delete(tmpdir)
+  
+  # add to DESCRIPTION file the version of litr used to create package:
+  litr:::write_version_to_description(package_dir)
+  
+  # add litr hash so we can tell later if package files were manually edited:
+  litr:::write_hash_to_description(package_dir)
 }
